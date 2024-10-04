b8c3845b11727a1f74fe7861bb1a3e65b6928b56
Thanapat1502 <psy150201027@gmail.com>
2024-10-04 12:55:51 +0700

    feat: answer ex1-ex6

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,9 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+for (let name of restaurants) {
+  name = "Restaurant Name: " + name;
+  newRestaurants.push(name);
+}
 
 console.log(newRestaurants);
